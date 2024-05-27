357b6b84887400f21b1b7002a0ba7a4f3fbb33f7
Guilherme Branco <gbranco@redhat.com>
2024-05-27 13:51:01 -0300

    OCM-8288 | fix: include changes to password generation in module

--- examples/rosa-hcp-public/main.tf
+++ examples/rosa-hcp-public/main.tf
@@ -39,6 +39,10 @@ module "htpasswd_idp" {
 resource "random_password" "password" {
   length  = 14
   special = true
+  min_lower = 1
+  min_numeric = 1
+  min_special = 1
+  min_upper = 1
 }
 
 ############################
