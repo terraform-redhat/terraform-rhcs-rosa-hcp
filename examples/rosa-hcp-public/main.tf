e529b2c1bb1ff3f7a2ae92cc76612284978f1c80
radtriste <tradisso@redhat.com>
2024-08-29 14:46:58 +0200

    Test 1.6.3-prerelease.1

--- examples/rosa-hcp-public/main.tf
+++ examples/rosa-hcp-public/main.tf
@@ -7,7 +7,8 @@ locals {
 # Cluster
 ############################
 module "hcp" {
-  source = "../../"
+  source = "terraform-redhat/rosa-hcp/rhcs"
+  version = "1.6.3-prerelease.1"
 
   cluster_name           = var.cluster_name
   openshift_version      = var.openshift_version
@@ -24,11 +25,34 @@ module "hcp" {
   operator_role_prefix  = local.operator_role_prefix
 }
 
+module "mp" {
+  source = "terraform-redhat/rosa-hcp/rhcs//modules/machine-pool"
+  version = "1.6.3-prerelease.1"
+
+  cluster_id = module.hcp.cluster_id
+
+  autoscaling = {
+    enabled = false
+    min_replicas = null
+    max_replicas = null
+  }
+  replicas = 2
+  auto_repair = true
+  name = "mp2"
+  openshift_version = var.openshift_version
+  subnet_id = module.vpc.private_subnets[0]
+  aws_node_pool = {
+    instance_type = "m5.xlarge"
+    tags = {}
+  }
+}
+
 ############################
 # HTPASSWD IDP
 ############################
 module "htpasswd_idp" {
-  source = "../../modules/idp"
+  source = "terraform-redhat/rosa-hcp/rhcs//modules/idp"
+  version = "1.6.3-prerelease.1"
 
   cluster_id         = module.hcp.cluster_id
   name               = "htpasswd-idp"
@@ -49,7 +73,8 @@ resource "random_password" "password" {
 # VPC
 ############################
 module "vpc" {
-  source = "../../modules/vpc"
+  source = "terraform-redhat/rosa-hcp/rhcs//modules/vpc"
+  version = "1.6.3-prerelease.1"
 
   name_prefix              = var.cluster_name
   availability_zones_count = 3
