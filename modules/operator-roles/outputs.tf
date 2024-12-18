359c26c34522d6585939b9297b85ace6702470ba
Guilherme Branco <gbranco@redhat.com>
2024-12-18 09:19:11 -0300

    OCM-12850 | feat: include shared vpc support and example

--- modules/operator-roles/outputs.tf
+++ modules/operator-roles/outputs.tf
@@ -7,3 +7,8 @@ output "operator_roles_arn" {
   value       = jsondecode(time_sleep.role_resources_propagation.triggers["operator_role_arns"])
   description = "List of Amazon Resource Names (ARNs) for all operator roles created."
 }
+
+output "shared_vpc_policy_attachments" {
+  value       = jsondecode(time_sleep.role_resources_propagation.triggers["shared_vpc_policy_attachments"])
+  description = "A list of Amazon Resource Names (ARNs) related to the shared VPC"
+}
