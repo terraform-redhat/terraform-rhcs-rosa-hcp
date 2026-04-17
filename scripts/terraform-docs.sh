618bf2475246592a7465a41cdd3c73be1aab2c0a
Amanda Hager Lopes de Andrade Katz <amanda.katz@redhat.com>
2026-04-17 14:47:08 -0300

    OCM-23911 | chore: change coderabbit config to use inheritance
    Signed-off-by: Amanda Hager Lopes de Andrade Katz <amanda.katz@redhat.com>

--- scripts/terraform-docs.sh
+++ scripts/terraform-docs.sh
@@ -6,14 +6,22 @@ TERRAFORM_DOCS_VERSION=v0.22.0
 BINARY=terraform-docs
 set +e
 INSTALLED_TERRADOCS_VERSION="$(terraform-docs --version | head -1 | cut -d' ' -f3)"
+PLATFORM=$( uname | tr '[:upper:]' '[:lower:]')
 set -e
 if [[ "$TERRAFORM_DOCS_VERSION" != "$INSTALLED_TERRADOCS_VERSION" ]]; then
-  curl -sSLo ./terraform-docs.tar.gz https://terraform-docs.io/dl/${TERRAFORM_DOCS_VERSION}/terraform-docs-${TERRAFORM_DOCS_VERSION}-$(uname)-amd64.tar.gz
-  tar -xzf terraform-docs.tar.gz terraform-docs
+  curl -sSLo ./terraform-docs-${TERRAFORM_DOCS_VERSION}-${PLATFORM}-amd64.tar.gz https://terraform-docs.io/dl/${TERRAFORM_DOCS_VERSION}/terraform-docs-${TERRAFORM_DOCS_VERSION}-${PLATFORM}-amd64.tar.gz
+  curl -sSLo ./terraform-docs.tar.gz.sha256sum https://terraform-docs.io/dl/${TERRAFORM_DOCS_VERSION}/terraform-docs-${TERRAFORM_DOCS_VERSION}.sha256sum
+  if sha256sum --ignore-missing -c "terraform-docs.tar.gz.sha256sum"; then
+    echo "File verification successful"
+  else
+    echo "File verification failed" >&2
+    exit 1
+  fi
+  tar -xzf terraform-docs-${TERRAFORM_DOCS_VERSION}-${PLATFORM}-amd64.tar.gz terraform-docs
   chmod +x terraform-docs
   echo "sudo required for moving terraform-docs to /usr/local/bin"
   sudo mv terraform-docs /usr/local/bin/terraform-docs
-  rm terraform-docs.tar.gz
+  rm terraform-docs-${TERRAFORM_DOCS_VERSION}-${PLATFORM}-amd64.tar.gz ./terraform-docs.tar.gz.sha256sum
 else
   echo "${BINARY} ${TERRAFORM_DOCS_VERSION} already installed - skipping install"
 fi
@@ -23,3 +31,4 @@ for d in . modules/* examples/*; do
   rm -rf $d/.terraform $d/.terraform.lock.hcl
   terraform-docs -c .terraform-docs.yml $d
 done
+
